b840fb5b6294105dfdf6947387ebd0ebfc5abfc3
hello <hello@example.com>
2022-03-12 22:52:30 +0530

    Show front default image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Card = props => {
   const { imgSource, imgDesc, className, onClick } = props;
+  const [sprite, setSprite] = useState('');
+
+  useEffect(() => {
+    if(imgSource) {
+      axios.get(imgSource).then(response => {
+        setSprite(response.data.sprites.front_default)
+      }).catch(error => console.log(error))
+    }
+
+  }, [imgSource])
+
   return (
     <div className={`grid-card ${className}`} onClick={onClick}>
       <img
         className={`img-thumbnail img-fluid grid-img`}
-        src={imgSource}
+        src={sprite}
         alt={imgDesc}
       />
     </div>
